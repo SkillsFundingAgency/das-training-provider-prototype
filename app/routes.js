5df8defc8518aa1db7431da3134bf5440e20f881
Gary Carr <imgarycarr@gmail.com>
2021-03-18 10:55:55 +0000

    Adding logic

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,17 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+//BULK UPLOADS
+
+
+//CSV upload or manual
+router.post('/bulk-upload/manual-or-upload', (req, res) => {
+	if(req.session.data['add-apprentice-option'] == 'added-manually'){
+		res.redirect('another-page')
+	} else {
+		res.redirect('file-upload')
+	}
+})
+
+
 module.exports = router
