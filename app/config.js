3a1c75896238d25365c92b78e41e929504914704
Hazel Fletcher <hazelfletcher@192.168.1.131>
2021-05-14 12:23:08 +0100

    prototype version update

--- app/config.js
+++ app/config.js
@@ -25,7 +25,7 @@ module.exports = {
   // Force HTTP to redirect to HTTPS on production
   useHttps: 'true',
 
-  // Cookie warning - update link to service's cookie page.
+// Cookie warning - update link to service's cookie page.
   cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
 
   // Enable or disable Browser Sync
