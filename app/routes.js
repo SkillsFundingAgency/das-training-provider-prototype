46c70ea9837e2b2b5a9edc63c74ec7fbf5240a0e
Hazel Fletcher <hazelfletcher@192.168.1.109>
2021-06-29 17:33:41 +0100

    v5 bulk upload

--- app/routes.js
+++ app/routes.js
@@ -31,12 +31,41 @@ router.post('/bulk-upload/v3/manual-or-upload', (req, res) => {
 	}
 })
 
+router.post('/bulk-upload/v4/manual-or-upload', (req, res) => {
+	if(req.session.data['add-apprentice-option'] == 'added-manually'){
+		res.redirect('manual-journey/manual')
+	} else {
+		res.redirect('file-upload')
+	}
+})
+
+
 //Check and confirm apprentice details
 router.post('/bulk-upload/file-upload-check', (req, res) => {
     if(req.session.data['bulk-upload-confirm'] == 'details-approve'){
         res.redirect('success')
+    } else if(req.session.data['bulk-upload-confirm'] == 'details-save'){
+        res.redirect('success-save-draft')
+	} else if(req.session.data['bulk-upload-confirm'] == 'details-new-file'){
+        res.redirect('amended-file')
+    }
+})
+
+router.post('/bulk-upload/v3/file-upload-check', (req, res) => {
+    if(req.session.data['bulk-upload-confirm'] == 'details-approve'){
+        res.redirect('success')
     } else if(req.session.data['bulk-upload-confirm'] == 'details-review'){
         res.redirect('success-review')
+    } else if(req.session.data['bulk-upload-confirm'] == 'details-save'){
+        res.redirect('success-save-draft')
+	} else if(req.session.data['bulk-upload-confirm'] == 'details-new-file'){
+        res.redirect('amended-file')
+    }
+})
+
+router.post('/bulk-upload/v4/file-upload-check', (req, res) => {
+    if(req.session.data['bulk-upload-confirm'] == 'details-approve'){
+        res.redirect('success')
     } else if(req.session.data['bulk-upload-confirm'] == 'details-save'){
         res.redirect('success-save-draft')
 	} else if(req.session.data['bulk-upload-confirm'] == 'details-new-file'){
@@ -53,8 +82,25 @@ router.post('/bulk-upload/amended-file', (req, res) => {
 	}
 })
 
+router.post('/bulk-upload/v4/amended-file', (req, res) => {
+	if(req.session.data['new-file'] == 'no'){
+		res.redirect('file-upload-check')
+	} else {
+		res.redirect('file-upload')
+	}
+})
+
+
 //Discard csv file
 router.post('/bulk-upload/discard-file', (req, res) => {
+	if(req.session.data['discard-file'] == 'no'){
+		res.redirect('file-upload-check')
+	} else {
+		res.redirect('success-discard-file')
+	}
+})
+
+router.post('/bulk-upload/v4/discard-file', (req, res) => {
 	if(req.session.data['discard-file'] == 'no'){
 		res.redirect('file-upload-check')
 	} else {
@@ -82,6 +128,16 @@ router.post('/bulk-upload/manual-journey/manual', (req, res) => {
 	}
 })
 
+//Manually add apprentice details
+router.post('/bulk-upload/v4/manual-journey/manual', (req, res) => {
+	if(req.session.data['manual-add'] == 'existing-cohort'){
+		res.redirect('existing-cohorts')
+	} else {
+		res.redirect('choose-employer')
+	}
+})
+
+
 //Confirm employer - Levy
 router.post('/bulk-upload/manual-journey/confirm-employer', (req, res) => {
 	if(req.session.data['create-cohort'] == 'yes'){
@@ -91,8 +147,24 @@ router.post('/bulk-upload/manual-journey/confirm-employer', (req, res) => {
 	}
 })
 
+router.post('/bulk-upload/v4/manual-journey/confirm-employer', (req, res) => {
+	if(req.session.data['create-cohort'] == 'yes'){
+		res.redirect('apprentice-details-add')
+	} else {
+		res.redirect('choose-employer')
+	}
+})
+
 //Confirm employer - Non-levy
 router.post('/bulk-upload/manual-journey/confirm-employer-non-levy', (req, res) => {
+	if(req.session.data['create-cohort-non-levy'] == 'yes'){
+		res.redirect('choose-reservation')
+	} else {
+		res.redirect('choose-employer')
+	}
+})
+
+router.post('/bulk-upload/v4/manual-journey/confirm-employer-non-levy', (req, res) => {
 	if(req.session.data['create-cohort-non-levy'] == 'yes'){
 		res.redirect('choose-reservation')
 	} else {
@@ -112,7 +184,7 @@ router.post('/cohort/index', (req, res) => {
 	}
 })
 
-//Ready for approval
+//0 apprentices
 router.post('/cohort/apprentices-0', (req, res) => {
 	if(req.session.data['send-employer'] == 'no'){
 		res.redirect('apprentice-requests')
